a4b053fe42984d6f650fb430edcb9fd52f41409f
citrono-lemon <citrono.lemon@gmail.com>
2023-02-09 17:03:48 +0900

    [CRE] Build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{ backgroundImage: "url(https://XXXdrive.google.com/uc?export=view&id=12WDgzUoeXt46G2CZZ0YyvHst6YYd3RlP)", backgroundPosition: "Right" }}>
         <Main />
         <NextScript />
       </body>
