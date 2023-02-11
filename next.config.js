acef2de833f83ffa6e2d34b902ffb720178306c5
citrono-lemon <citrono.lemon@gmail.com>
2023-02-12 03:54:14 +0900

    [UPD] Build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
+
 const nextConfig = {
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
   reactStrictMode: true,
+  publicRuntimeConfig: { urlPrefix },  // ★コレ
 }
 
 module.exports = nextConfig
