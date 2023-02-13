3538d0b39efe6af6d3ef152023fe72938f1eceb2
citrono-lemon <citrono.lemon@gmail.com>
2023-02-13 20:32:12 +0900

    [UPD] Build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="Arbara Software" />
       <body style={{ backgroundImage: "url(https://XXXdrive.google.com/uc?export=view&id=12WDgzUoeXt46G2CZZ0YyvHst6YYd3RlP)", backgroundPosition: "Right" }}>
         <Main />
         <NextScript />
